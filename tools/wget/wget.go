c8e84e5506a5ec71e864694ac60dc80c5553087c
czxichen <czxichen@163.com>
2016-04-27 09:15:31 +0800

    Signed-off-by: czxichen <czxichen@163.com>

--- tools/wget/wget.go
+++ tools/wget/wget.go
@@ -2,20 +2,37 @@ package wget
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"io"
 	"net/http"
 	"os"
 )
 
-func client() *http.Client {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+func client(rootCa, rootKey string) *http.Client {
+	var tr *http.Transport
+	certs, err := tls.LoadX509KeyPair(rootCa, rootKey)
+	if err != nil {
+		tr = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+	} else {
+		ca, err := x509.ParseCertificate(certs.Certificate[0])
+		if err != nil {
+			return &http.Client{Transport: tr}
+		}
+		pool := x509.NewCertPool()
+		pool.AddCert(ca)
+
+		tr = &http.Transport{
+			TLSClientConfig: &tls.Config{RootCAs: pool},
+		}
+
 	}
 	return &http.Client{Transport: tr}
 }
 
-func Wget(url, name string) (bool, error) {
-	resp, err := client().Get(url)
+func Wget(url, name, rootCa, rootKey string) (bool, error) {
+	resp, err := client(rootCa, rootKey).Get(url)
 	if err != nil {
 		return false, err
 	}
